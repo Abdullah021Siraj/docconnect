6d169c7a9ce84275427c78bc585b7ad74f490898
Taha Sheikh <tahashayk2k@gmail.com>
2025-04-12 20:09:05 +0500

    [MODIFIED] ADDED Meeting

---
+++ src/app/meeting/[id]/page.tsx
@@ -0,0 +1,18 @@
+
+"use client"
+import dynamic from 'next/dynamic'
+
+const Message = dynamic(
+    () => import('./agora2/authenticationWorkflowManager'),
+    {ssr: false})
+
+const IndexPage = () => {
+    return (
+        <>
+            <Message/>
+        </>
+    )
+}
+
+export default IndexPage
+
